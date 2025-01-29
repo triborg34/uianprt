361f7373b7fc699dcaa42f55310e0e081f4df6f5
triborg34 <you@example.com>
2025-01-29 14:28:34 +0330

    production 2

--- lib/widgets/arvand_pelak.dart
+++ lib/widgets/arvand_pelak.dart
@@ -29,7 +29,7 @@ class ArvandPelak extends StatelessWidget {
               Container(
                 padding: EdgeInsets.symmetric(horizontal: 25),
                 child: Text(
-                  "${entry.plateNum!.toPersianDigit().substring(0,entry.plateNum!.length-2)} | ${entry.plateNum!.toPersianDigit().substring(entry.plateNum!.length-2)}",
+                  entry.plateNum!.length <=5 ? entry.plateNum!.toPersianDigit() : "${entry.plateNum!.toPersianDigit().substring(0,entry.plateNum!.length-2)} | ${entry.plateNum!.toPersianDigit().substring(entry.plateNum!.length-2)}",
                   style: TextStyle(color: Colors.black, fontSize: 18),
                   
                 ),
